2a8e4870818755073246b6d584cdae64a9eed2c7
kushal-unjiya <kushalunjiya01@gmail.com>
2024-03-14 10:53:30 +0530

    updated

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -14,12 +14,15 @@ router.post('/uploadFile', upload.single('file'), async (req, res) => {
     if (!req.file) {
       return res.status(400).json({ message: 'No file uploaded.' });
     }
-
+    console.log('File received:', req.file);
     const workbook = xlsx.read(req.file.buffer, { type: 'buffer' });
+    console.log('Workbook:', workbook);
+
     const jsonData = xlsx.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]]);
 
     await Student.insertMany(jsonData);
-
+    console.log('Data inserted successfully.');
+    
     res.json({ message: 'Data imported successfully.' });
   } catch (error) {
     console.error('Error processing file:', error);
